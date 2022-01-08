674cbe46ce9c68bd5748c6941ac26a7d3b9f52f8
Ryan Yandle <ryany@microsoft.com>
2022-01-08 15:45:54 -0800

    add theme serialization UT

--- src/GCRealTimeMon/Configuration/Theme/CustomTheme.cs
+++ src/GCRealTimeMon/Configuration/Theme/CustomTheme.cs
@@ -1,8 +1,11 @@
 ﻿namespace realmon.Configuration.Theme
 {
+    using YamlDotNet.Serialization;
     /// <summary>
     /// A custom user-defined theme. If a particular property is not set, it falls back to the default.
     /// </summary>
+    /// <remarks>Some of the members are specially annoated with <see cref="YamlMemberAttribute"/> because their default naming
+    /// convention would be more onerous.</remarks>
     public class CustomTheme : ITheme
     {
         private string gCTableHeaderColor;
@@ -20,54 +23,63 @@
         private string totalHeapColor;
         private string warningColor;
 
+        [YamlMember(Alias = "gc_table_header_color")]
         public string GCTableHeaderColor
         {
             get => gCTableHeaderColor ?? ThemeConfig.Default.GCTableHeaderColor;
             set => gCTableHeaderColor = value;
         }
 
+        [YamlMember(Alias = "gen0_heap_color")]
         public string Gen0HeapColor
         {
             get => gen0HeapColor ?? ThemeConfig.Default.Gen0HeapColor;
             set => gen0HeapColor = value;
         }
 
+        [YamlMember(Alias = "gen0_row_color")]
         public string Gen0RowColor
         {
             get => gen0RowColor ?? ThemeConfig.Default.Gen0RowColor;
             set => gen0RowColor = value;
         }
 
+        [YamlMember(Alias = "gen1_heap_color")]
         public string Gen1HeapColor
         {
             get => gen1HeapColor ?? ThemeConfig.Default.Gen1HeapColor;
             set => gen1HeapColor = value;
         }
 
+        [YamlMember(Alias = "gen1_row_color")]
         public string Gen1RowColor
         {
             get => gen1RowColor ?? ThemeConfig.Default.Gen1RowColor;
             set => gen1RowColor = value;
         }
 
+        [YamlMember(Alias = "gen2_heap_color")]
         public string Gen2HeapColor
         {
             get => gen2HeapColor ?? ThemeConfig.Default.Gen2HeapColor;
             set => gen2HeapColor = value;
         }
 
+        [YamlMember(Alias = "gen2_row_color")]
         public string Gen2RowColor
         {
             get => gen2RowColor ?? ThemeConfig.Default.Gen2RowColor;
             set => gen2RowColor = value;
         }
 
+        [YamlMember(Alias = "gen3_heap_color")]
         public string Gen3HeapColor
         {
             get => gen3HeapColor ?? ThemeConfig.Default.Gen3HeapColor;
             set => gen3HeapColor = value;
         }
 
+        [YamlMember(Alias = "gen4_heap_color")]
         public string Gen4HeapColor
         {
             get => gen4HeapColor ?? ThemeConfig.Default.Gen4HeapColor;
