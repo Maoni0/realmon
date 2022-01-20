94eab22f37d7ff31eb58571436a7cd0c9c74fc22
Ryan Yandle <ryandle@gmail.com>
2022-01-19 16:46:44 -0800

    Prettify console output using Spectre.Console (#39)
    * commiting prototype
    
    * Initial cleanup
    
    * fix spacing + bad merge
    
    * Cleanup
    
    * formatting cleanup
    
    * take latest stable spectre.console
    
    * cleanup based on review comments
    
    * - Break theme out into its own subfolder. Introduce light and dark theme defaults. Add CustomTheme that can be defined in yaml and falls back to the default theme when specific properties aren't defined. The default theme is dark, but switches to light if the console background color is White.
    - Changed LiveOutputTable.WriteRowAsync to be synchronous
    - Cleaned up use of wrong namespace and made it the default for the csproj
    - Added two basic UTs; more to come;
    
    * add theme serialization UT
    
    * - Refactored capturing TraceGC event data into a single object so we can pass that reference around and not worry about tearing between the datetime & TraceGC data.
    - Introduced a new interface for console output responsibilities.Factored the Spectre code into one implementation. Will move the plaintext code back into a 2nd implementation. We'll have a factory method choose the implementation at runtime based on whether output is redirected && config.
    - Got rid of Restart in LiveOutput Table...Start/Stop should be enough. Added a check to prevent starting twice.
    
    * - Implemented PlainText console output (brings back the original implementation)
    - Added Factory to decide output based on yaml config or console output being redirected
    - Trimmed the IConsoleOuput interface to have only what is needed by Program.cs
    
    * Cleanup usings to match existing style
    
    * add documentation
    
    * fixing spacing after merge
    
    * fix typo
    
    * Remove Rule color, use MessageColor instead.

---
+++ src/GCRealTimeMon/Utilities/ConsoleOutFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using realmon.Configuration.Theme;
+
+namespace realmon.Utilities
+{
+    using Configuration = realmon.Configuration.Configuration;
+
+    internal static class ConsoleOutFactory
+    {
+        internal static IConsoleOut Create(Configuration configuration)
+        {
+            if (Console.IsOutputRedirected || configuration.Theme?.UsePlainText == true)
+            {
+                return new PlainTextConsoleOut(configuration);
+            }
+
+            ThemeConfig.Initialize(configuration);
+            return new SpectreConsoleOut(configuration);
+        }
+    }
+}
